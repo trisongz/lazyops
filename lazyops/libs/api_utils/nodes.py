b49d5a93259de4d73343e40efd203a8a17160b87
trisongz <4735784+trisongz@users.noreply.github.com>
2023-04-15 01:53:22 -0500

    update

--- lazyops/libs/api_utils/nodes.py
+++ lazyops/libs/api_utils/nodes.py
@@ -18,8 +18,18 @@ class NodeProcess(BaseModel):
     exe: str # Command Line
     cmdline: List[str]
 
+    is_worker: Optional[bool] = None
+    has_workers: Optional[bool] = None
+
     parent: Optional[psutil.Process] = None
     linked: Optional[List[psutil.Process]] = []
+
+    # Context vars
+    runtime: Optional[str] = None
+    entrypoint: Optional[str] = None
+    exact_match: Optional[bool] = None
+    worker_entrypoint: Optional[str] = None
+
 
     @lazyproperty
     def parent_pid(self) -> Optional[int]:
@@ -70,7 +80,8 @@ class NodeProcess(BaseModel):
 
     @staticmethod
     def get_stripped_cmdline(
-        cmdline: List[str]
+        cmdline: List[str],
+        has_workers: bool = False,
     ) -> str:
         """
         Strips the current process from the cmdline
@@ -80,6 +91,9 @@ class NodeProcess(BaseModel):
         for cmd_arg in cmdline:
             if 'pipe_handle' in cmd_arg:
                 cmd_arg = cmd_arg.split(', pipe_handle', 1)[0] + ')'
+                # will fix this later
+                if has_workers and 'tracker_fd' in cmd_arg:
+                    cmd_arg = '(' + cmd_arg.split('(tracker_fd', 1)[0] + ')'
             cmd += f'|{cmd_arg}' 
         return cmd
 
@@ -90,6 +104,7 @@ class NodeProcess(BaseModel):
         and removes params that are not needed
         """
         return self.get_stripped_cmdline(self.cmdline)
+        # return self.get_stripped_cmdline(self.cmdline, has_workers = self.has_workers)
     
     @lazyproperty
     def total_processes(self) -> int:
@@ -111,14 +126,17 @@ class NodeProcess(BaseModel):
             pids.remove(self.parent_pid)
         return sorted(pids, key=int)
 
-    @lazyproperty
+    # @lazyproperty
+    @property
     def leader_pid(self) -> Optional[int]:
         """
         Returns the leader pid
         """
+        if self.is_worker: return self.sorted_linked_pids[1]
         return self.parent_pid if self.is_parent else self.sorted_linked_pids[0]
     
-    @lazyproperty
+    # @lazyproperty
+    @property
     def is_leader(self) -> bool:
         """
         If the current process is the leader
@@ -135,10 +153,15 @@ class NodeProcess(BaseModel):
         runtime: Optional[str] = "uvicorn",
         entrypoint: Optional[str] = "main:app",
         exact_match: Optional[bool] = False,
+        is_worker: Optional[bool] = False,
+        worker_entrypoint: Optional[str] = None,
+        has_workers: Optional[bool] = False,
     ) -> 'NodeProcess':
         """
         Utility Function to get the current process information
         of the current node / worker
+
+        can probably improve the logic but it works for now.
         """
         node_pid = pid or os.getpid()
         node_process = psutil.Process(node_pid)
@@ -148,6 +171,14 @@ class NodeProcess(BaseModel):
             name = node_process.name(),
             exe = node_process.exe(),
             cmdline = node_process.cmdline(),
+            has_workers = has_workers,
+            is_worker = is_worker,
+
+            # Context vars
+            runtime = runtime,
+            entrypoint = entrypoint,
+            exact_match = exact_match,
+            worker_entrypoint = worker_entrypoint,
         )
 
         for p in psutil.process_iter(attrs=["pid", "name", "exe", "cmdline"]):
@@ -157,15 +188,50 @@ class NodeProcess(BaseModel):
             # Check that the process name is identical to the node
             if p.info['exe'] != process.exe:
                 continue
-            
-            if any(runtime in cmd_arg for cmd_arg in p.info["cmdline"]) and \
-                any((entrypoint == cmd_arg if exact_match else entrypoint in cmd_arg) for cmd_arg in p.info["cmdline"]):
+
+            if not is_worker and \
+                any(runtime in cmd_arg for cmd_arg in p.info["cmdline"]) and \
+                    any((entrypoint == cmd_arg if exact_match else entrypoint in cmd_arg) for cmd_arg in p.info["cmdline"]):
+                
                 process.linked.append(p)
                 process.parent = p
                 continue
 
+
+            if (
+                is_worker
+                and worker_entrypoint
+                and all(runtime not in cmd_arg for cmd_arg in p.info["cmdline"])
+                and any(
+                    (
+                        worker_entrypoint == cmd_arg
+                        if exact_match
+                        else worker_entrypoint in cmd_arg
+                    )
+                    for cmd_arg in p.info["cmdline"]
+                )
+                and not process.parent
+            ):
+                process.parent = p
+                continue
+
+
+            # if process.is_multiprocess and process.stripped_cmdline == cls.get_stripped_cmdline(p.info["cmdline"], has_workers = has_workers):
             if process.is_multiprocess and process.stripped_cmdline == cls.get_stripped_cmdline(p.info["cmdline"]):
                 process.linked.append(p)
                 continue
-
+        
         return process
+    
+    def reconfigure(self):
+        """
+        Reconfigures the current process
+        """
+        self = self.get_process(
+            pid = self.pid,
+            runtime = self.runtime,
+            entrypoint = self.entrypoint,
+            exact_match = self.exact_match,
+            is_worker = self.is_worker,
+            worker_entrypoint = self.worker_entrypoint,
+        )
