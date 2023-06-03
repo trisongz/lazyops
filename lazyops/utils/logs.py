20e663b1f4829974512dbcf09d59059f025c24ac
trisongz <4735784+trisongz@users.noreply.github.com>
2023-06-02 23:02:12 -0500

    hotfix for logging

--- lazyops/utils/logs.py
+++ lazyops/utils/logs.py
@@ -278,7 +278,7 @@ class InterceptHandler(logging.Handler):
         try:
             level = logger.level(record.levelname).name
         except ValueError:
-            level = self.loglevel_mapping[record.levelno]
+            level = self.loglevel_mapping.get(record.levelno, 'INFO')
         # if "Unclosed client session" in record.message:
         #     print('Has unclosed client session')
         #     return
