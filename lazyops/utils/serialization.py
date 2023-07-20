a9b790a373258963486d9ae74cc18d5c839a16ea
trisongz <4735784+trisongz@users.noreply.github.com>
2023-07-20 18:55:55 -0500

    Update serialization.py

--- lazyops/utils/serialization.py
+++ lazyops/utils/serialization.py
@@ -373,6 +373,54 @@ try:
             obj: typing.Dict[typing.Any, typing.Any], 
             *args, 
             default: typing.Dict[typing.Any, typing.Any] = None, 
+            cls: typing.Type[json.JSONEncoder] = None,
+            _fallback_method: typing.Optional[typing.Callable] = None,
+            **kwargs
+        ) -> str:
+            """
+            Serializes a dict into a JSON string
+            """
+            try:
+                return simdjson.dumps(obj, *args, default = default, cls = cls, **kwargs)
+            except Exception as e:
+                if _fallback_method is not None:
+                    return _fallback_method(obj, *args, default = default, **kwargs)
+                raise e
+
+        @staticmethod
+        def loads(
+            data: typing.Union[str, bytes], 
+            *args, 
+            object_hook: typing.Optional[typing.Callable] = None,
+            recursive: typing.Optional[bool] = True,
+            _raw: typing.Optional[bool] = False,
+            _fallback_method: typing.Optional[typing.Callable] = None,
+            **kwargs
+        ) -> typing.Union[typing.Dict[typing.Any, typing.Any], typing.List[str], simdjson.Object, simdjson.Array]:
+            """
+            Loads a JSON string into a dict using the ObjectModelDecoder
+            """
+            try:
+                value = _parser.parse(data, recursive)
+                return value if _raw or not object_hook else object_hook(value)
+            
+            except Exception as e:
+                if _fallback_method is not None:
+                    return _fallback_method(data, *args, **kwargs)
+                raise e
+
+
+    class SimdJsonModelSerializer:
+
+        """
+        JSON Encoder and Decoder using simdjson
+        """
+
+        @staticmethod
+        def dumps(
+            obj: typing.Dict[typing.Any, typing.Any], 
+            *args, 
+            default: typing.Dict[typing.Any, typing.Any] = None, 
             cls: typing.Type[json.JSONEncoder] = ObjectModelEncoder,
             _fallback_method: typing.Optional[typing.Callable] = None,
             **kwargs
@@ -396,7 +444,7 @@ try:
             _raw: typing.Optional[bool] = False,
             _fallback_method: typing.Optional[typing.Callable] = None,
             **kwargs
-        ) -> typing.Union[typing.Dict[typing.Any, typing.Any], typing.List[str], simdjson.Object, simdjson.Array]:
+        ) -> typing.Union[typing.Dict[typing.Any, typing.Any], typing.List[str], 'BaseModel', simdjson.Object, simdjson.Array]:
             """
             Loads a JSON string into a dict using the ObjectModelDecoder
             """
@@ -414,7 +462,8 @@ try:
 except ImportError:
     _parser = None
 
-    SimdJson = JsonModelSerializer
+    SimdJson = Json
+    SimdJsonModelSerializer = JsonModelSerializer
 
 
 
