af4d98d2aed222ee996388040a746ae7b65cd634
trisongz <4735784+trisongz@users.noreply.github.com>
2024-02-06 12:23:43 -0600

    Update parser.py

--- lazyops/libs/abcs/cli/parser.py
+++ lazyops/libs/abcs/cli/parser.py
@@ -1,3 +1,91 @@
+from __future__ import annotations
+
 """
 Parser ABC
 """
+
+import argparse
+from typing import Any, Dict, List, Optional, Tuple, Union, Type, Set, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from pydantic import BaseModel
+    from pydantic.fields import FieldInfo
+
+
+_parsed_parser_objects: Dict[str, argparse.ArgumentParser] = {}
+
+_parser_types: Dict[str, Type] = {
+    'str': str,
+    'int': int,
+    'float': float,
+    'bool': bool,
+    'list': list,
+    'dict': dict,
+    # 'tuple': tuple,
+    # 'None': None,
+}
+
+def parse_one(
+    name: str,
+    field: FieldInfo,
+) -> Tuple[Set, Dict]:
+    """
+    Parse a single field into the args and kwargs for the add_argument method
+    """
+    args = set()
+    kwargs = {}
+
+    field_type = field.annotation
+    for pt in _parser_types.values():
+        if field_type == Optional[pt]:
+            field_type = pt
+            break
+
+    if field.is_required():
+        args.add(name)
+    else:
+        args.add(f'--{name}')
+        if len(name) > 3:
+            if '_' in name:
+                parts = name.split('_')
+                short_name = ''.join([p[0] for p in parts])
+                args.add(f'-{short_name}')
+            else: args.add(f'-{name[:2]}')
+
+
+    if field_type == bool:
+        kwargs['action'] = 'store_true' if field.default is False else 'store_false'
+    elif field_type == list:
+        kwargs['nargs'] = '+'
+    else:
+        kwargs['type'] = field_type
+
+    if field.default is not None:
+        kwargs['default'] = field.default
+    elif field.default_factory is not None:
+        kwargs['default'] = None
+
+    if field.description is not None:
+        kwargs['help'] = field.description
+
+    return args, kwargs
+
+
+def create_argparser_from_model(
+    model: Type['BaseModel']
+) -> argparse.ArgumentParser:
+    """
+    Create an Argument Parser from a Pydantic Model
+    """
+    global _parsed_parser_objects
+    model_key = f'{model.__module__}.{model.__name__}'
+    if model_key not in _parsed_parser_objects:
+        parser = argparse.ArgumentParser(description=model.__doc__)
+        for name, field in model.model_fields.items():
+            if field.json_schema_extra and field.json_schema_extra.get('hidden', False):
+                continue
+            args, kwargs = parse_one(name, field)
+            parser.add_argument(*args, **kwargs)
+        _parsed_parser_objects[model_key] = parser
+    return _parsed_parser_objects[model_key]
+    
